de78f2cd99d5bfcfcac5650ee15993361f61e704
Yuvasri-10 <71762307062@cit.edu.in>
2025-06-11 10:50:44 +0530

    Update services.js

--- src/pages/services.js
+++ src/pages/services.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import Link from 'next/link';
 
 const defaultTasks = {
   professional: ["Finish project report", "Attend team meeting", "Send client follow-up email", "Review quarterly goals"],
