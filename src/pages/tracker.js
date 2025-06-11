44ef507b6eee4daf78f0c0c6aa672a6ece73e4d2
Yuvasri-10 <71762307062@cit.edu.in>
2025-06-11 11:11:31 +0530

    Update tracker.js

--- src/pages/tracker.js
+++ src/pages/tracker.js
@@ -11,10 +11,10 @@ const Tracker = () => {
         <p>Your Monthly Productivity Tracker</p>
         <nav>
           <Link href="/">Home</Link>
-  <Link href="/services">Planner</Link>
-  <Link href="/tracker">Tracker</Link>
-  <Link href="/about">About Us</Link>
-  <Link href="/contact">Contact Us</Link>
+          <Link href="/services">Planner</Link>
+          <Link href="/tracker">Tracker</Link>
+          <Link href="/about">About Us</Link>
+          <Link href="/contact">Contact Us</Link>
         </nav>
       </header>
 
